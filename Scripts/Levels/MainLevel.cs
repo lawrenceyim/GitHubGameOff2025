a1b9805783c8260f42f8a502a138857a9b5ad48d
lawrenceyim <lawrence.yim.dev@gmail.com>
2025-11-06 13:43:49 -0500

    Added duration to each wave.

--- Scripts/Levels/MainLevel.cs
+++ Scripts/Levels/MainLevel.cs
@@ -29,6 +29,11 @@ public partial class MainLevel : Node2D, IInputState {
     [Export]
     private float _horizontalMoveSpeed; // Per physic tick
 
+    [Export]
+    private int _secondsPerWave; 
+
+    private int _ticksLeftInWave;
+    
     private CollectibleManager _collectibleManager;
     private const string _moveLeft = "A";
     private const string _moveRight = "D";
@@ -40,7 +45,7 @@ public partial class MainLevel : Node2D, IInputState {
     };
 
     public override void _Ready() {
-        InstantiateLevel();
+        _InstantiateLevel();
     }
 
     public void ProcessInput(InputEventDto dto) {
@@ -49,18 +54,14 @@ public partial class MainLevel : Node2D, IInputState {
         }
     }
 
-    public override void _Process(double delta) {
-        base._Process(delta);
-        // update Horse UI
-    }
-
     public override void _PhysicsProcess(double delta) {
         base._PhysicsProcess(delta);
 
-        MovePlayer();
+        _MovePlayer();
+        _TickDownTimer();
     }
 
-    private void InstantiateLevel() {
+    private void _InstantiateLevel() {
         _serviceLocator = GetNode<ServiceLocator>(ServiceLocator.AutoloadPath);
         InputStateMachine inputStateMachine = _serviceLocator.GetService<InputStateMachine>(ServiceName.InputStateMachine);
         inputStateMachine?.SetState(this);
@@ -73,16 +74,26 @@ public partial class MainLevel : Node2D, IInputState {
         _collectibleManager?.Initialize(shrimpRepository, shrimpPackedScene, _collectibleLeftXBound,
             _collectibleRightXBound, _collectibleYSpawnPosition, _player.Position.Y);
 
-        _collectibleOutOfBoundsArea.AreaEntered += DestroyCollectible;
+        _collectibleOutOfBoundsArea.AreaEntered += _DestroyCollectible;
+        
+        _ticksLeftInWave = _secondsPerWave * Engine.PhysicsTicksPerSecond;
     }
 
-    private void DestroyCollectible(Area2D area) {
+    private void _TickDownTimer() {
+        _ticksLeftInWave--;
+        if (_ticksLeftInWave == 0) {
+            GD.Print("End of wave");
+            // Go to end of day screen
+        }
+    }
+
+    private void _DestroyCollectible(Area2D area) {
         if (area.GetParent() is Shrimp shrimp) {
             _collectibleManager.DestroyShrimp(shrimp.GetId());
         }
     }
 
-    private void MovePlayer() {
+    private void _MovePlayer() {
         Vector2 newPosition = Vector2.Zero;
         newPosition.X = (_keyPressed[_moveLeft] ? -1 : 0) + (_keyPressed[_moveRight] ? 1 : 0);
         newPosition *= _horizontalMoveSpeed;
